52a23a27a8a97d04c9c482147d10733c6e74a74a
Søren Skovsbøll <soren@skovsboll.com>
2018-09-14 17:36:19 +0200

    switch to msgpack for serialization

---
+++ lib/seeder.rb
@@ -0,0 +1,21 @@
+require "./lib/bloomed"
+
+KB = 2 ** 10
+
+module Seeder
+  def self.seed(all: false, cache_dir: nil)
+    ps = [0.01, 0.001, 0.0001]
+    cs = [1E4, 1E5]
+
+    if all
+      cs += [1E6, 1E7, 1E8]
+    end
+
+    cs.product(ps).each do |c, p|
+      print "Generating filter for top #{c.to_i} with #{p} precision "
+      b = Bloomed::PW.new(top: c, false_positive_probability: p, cache_dir: cache_dir)
+      size = File.size(b.filename)
+      puts "Done [#{size / KB} kb]"
+    end
+  end
+end
--- spec/bloomed_spec.rb
+++ spec/bloomed_spec.rb
@@ -1,37 +1,72 @@
 RSpec.describe Bloomed do
-  it 'has a version number' do
+  it "has a version number" do
     expect(Bloomed::VERSION).not_to be nil
   end
 
-  context 'low precision' do
+  context "low precision" do
     subject { Bloomed::PW.new(top: 1E4, false_positive_probability: 0.01) }
 
-    it 'detects password123' do
-      expect('password123').to be_pwned
+    it "detects password123" do
+      expect("password123").to be_pwned
     end
 
-    it 'does not detect password200' do
-      expect('password200').not_to be_pwned
+    it "does not detect password200" do
+      expect("password200").not_to be_pwned
     end
 
-    it 'does not detect o9wreiour' do
-      expect('o9wreiour').not_to be_pwned
+    it "does not detect o9wreiour" do
+      expect("o9wreiour").not_to be_pwned
     end
   end
 
-  context 'high precision' do
+  context "high precision" do
     subject { Bloomed::PW.new(top: 1E5, false_positive_probability: 0.001) }
 
-    it 'detects password123' do
-      expect('password123').to be_pwned
+    it "detects password123" do
+      expect("password123").to be_pwned
     end
 
-    it 'detects password20' do
-      expect('password20').to be_pwned
+    it "detects password20" do
+      expect("password20").to be_pwned
     end
 
-    it 'does not detect o9wreiour' do
-      expect('o9wreiour').not_to be_pwned
+    it "does not detect o9wreiour" do
+      expect("o9wreiour").not_to be_pwned
+    end
+  end
+
+  context "msgpack" do
+    subject { Bloomed::PW.new(top: 1E5, false_positive_probability: 0.001) }
+
+    it "serializes" do
+      data = subject.to_msgpack
+    end
+
+    it "deserializes" do
+      subject2 = Bloomed::PW.from_msgpack(subject.to_msgpack)
+      expect(subject2.top).to eq subject.top
+      expect(subject2.false_positive_probability).to eq subject.false_positive_probability
+      expect(subject2.pwned?("password20")).to eq true
+    end
+
+    context "overriding the cache dir" do
+      context "empty dir" do
+        require 'tempfile'
+        subject { Bloomed::PW.new(top: 1E2, false_positive_probability: 0.01, cache_dir: File.dirname(Tempfile.new)) }
+        it('raises') { expect{ subject }.to raise_error(IOError) }
+      end
+
+      context "non-empty dir" do
+        it('loads') do
+          require 'tempfile'
+          b = Bloomed::PW.new(top: 1E4, false_positive_probability: 0.01)
+          dir = File.dirname(Tempfile.new)
+          new_file = File.join(dir, File.basename(b.filename))
+          FileUtils.cp(b.filename, new_file)
+          b2 = Bloomed::PW.new(top: 1E4, false_positive_probability: 0.01, cache_dir: dir)
+          expect(b2.filename).not_to eq b.filename
+        end
+      end
     end
   end
 end
